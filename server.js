29ef7fcc20f7817281db150473582c83cf146f40
unknown <kuntalstar@gmail.com>
2016-12-18 15:24:56 +0530

    yu

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var app=express();
 app.use('/app',express.static(__dirname+'/app'));
+app.use('/rsrc/unity/lib/sdk_6.2/CanvasFacebook.dll',express.static(__dirname+'/rsrc/unity/lib/sdk_6.2/CanvasFacebook.dll'));
 app.get('/',function(req,res){
 res.sendFile(__dirname+'/index.html');
 
